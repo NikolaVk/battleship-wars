7e4869fedb5ff14c7d0de7035fece4588099f3f2
NikolaVk <nikola@ique.nl>
2021-09-15 21:18:59 +0200

    Adding player and oponent random boat generating function

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+import random
+
 cellState_unknown = 0
 cellState_boat = 1
 
-
 player_grid = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 oponent_grid = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+player_boats = []
+oponent_boats = []
+boat_sizes = [5, 4, 3, 2]
 
 
 grid_chars = ["[ ]", " X ", " X ", " o "]
@@ -30,4 +34,43 @@ def draw_grid(player_states, oponent_states):
         print(row_string)
 
 
-draw_grid(player_grid, oponent_grid)
+
+def init_grid(grid):
+    """
+    Randomly generates ships for player
+    and opponent and makes sure its inside
+    the grid and that the boats do not overlap
+    """
+    # position the boats
+    for boat_index in range(4):
+        # Place the boat
+        orientation = random.randint(0, 1)
+
+        while True:
+            if orientation == 0:
+                # horizontal
+                row_index = random.randint(0, 7)
+                col_index = random.randint(0, 7 - boat_sizes[boat_index])
+                # check if we are not overriding existing boats
+                hits = False
+                for colcelloffset in range(0, boat_sizes[boat_index]):
+                    if grid[row_index][colcelloffset + col_index] != cellState_unknown:
+                        # spot already occupied
+                        hits = True
+                if hits is False:
+                    for colcelloffset in range(0, boat_sizes[boat_index]):
+                        grid[row_index][colcelloffset + col_index] = cellState_boat
+                    break
+            else:
+                # vertical
+                row_index = random.randint(0, 7 - boat_sizes[boat_index])
+                col_index = random.randint(0, 7)
+                hits = False
+                for rowcelloffset in range(0, boat_sizes[boat_index]):
+                    if grid[row_index + rowcelloffset][col_index] != cellState_unknown:
+                        # spot already occupied
+                        hits = True
+                if hits is False:
+                    for rowcelloffset in range(0, boat_sizes[boat_index]):
+                        grid[row_index + rowcelloffset][col_index] = cellState_boat
+                    break
